02024c094e8decd23d6abb5601292045bd02885a
Soren Rood <sorenrood@gmail.com>
2021-01-19 08:48:42 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "";
+            Console.WriteLine("Enter your age.");
+            x = Console.ReadLine();
+            Console.WriteLine($"You are {x} years old.");
+        }
+    }
+}
